7ff006298f5a6935760c88b9e90bf505c62a9450
Florian <office@tntradio.ro>
2018-04-27 21:04:35 +0300

    Update YoutubeTitle.tcl

--- YoutubeTitle.tcl
+++ YoutubeTitle.tcl
@@ -1,17 +1,17 @@
 ##########################################################################################
-# Youtube Title 1.6
+# Youtube Title 1.7
 # - fetches and displays video information when a YouTube link is posted in channel.
 # - displays title, date and rating of posted video links. 
 # - supports also HTTPS links.
 #
-# requires: package http 
+# requires: packages http 
 #
 # UPDATES/CHANGES:
-# - Now with multiple output methods of showing the infos configurable via channel
-# - Now with multi-language support configurable via channel
-# - Fixed entity decoding problems in return titles
-# - Now supports eggdrop version less than 1.8.*
-# - All options/settings are now case sensitive
+# - (1.7) shows and infos about duration/length
+# - (1.6) multiple output methods of showing the infos configurable via channel
+# - (1.6) multi-language support configurable via channel
+# - (1.6) supports eggdrop version less than 1.8.*
+# - (1.6) options/settings are now case sensitive
 #
 # To activate .chanset #channel +ytitle | BlackTools : .set +ytitle
 #
@@ -44,10 +44,11 @@ bind ctcp - ACTION check:youtube:me
 # - to change script language:
 # .set ytlang <ro/en/fr/es/it> or .chanset #channel ytlang <ro/en/fr/es/it>
 #
-# - to set script color:
-# .set ytcolor <0/1> (1 - colors ; 0 - no colors)
+# - to set script color
+# .set ytcolor <0/1> ; 1 - colors ; 0 - no colors
 #
 ###
+
 setudef flag ytitle
 setudef str ytlang
 setudef str ytcolor
@@ -118,6 +119,10 @@ if {$views == ""} { set views "N/A" }
 	set dontlike [concat [lindex $output 3]]
 	set bywho [concat [lindex $output 4]]
 	set update [lindex $output 5]
+	set duration [lindex $output 6]
+	set duration [string map {"PT" ""} $duration]
+	set minutes [lindex [split $duration "M"] 0]
+	set seconds [string map {"S" ""} [lindex [split $duration "M"] 1]]
 if {$novideo == "1"} {
 	youtube:tell $nick $chan 1
 	return
@@ -125,9 +130,10 @@ if {$novideo == "1"} {
 	set scan [clock scan $update -format {%Y-%m-%d}]
 	set update [clock format $scan -format {%d/%m/%Y}]
 	
-	youtube:tell $nick $chan "$title~$bywho~$views~$likes~$dontlike~$update"
+	youtube:tell $nick $chan "$title~$bywho~$views~$likes~$dontlike~$update~$minutes~$seconds"
 
 }
+
 
 proc youtube:tell {nick chan arg} {
 	global black ytitle
@@ -188,38 +194,38 @@ check:youtube $nick $host $hand $chan $arg
 set ytitle(projectName) "Youtube Title"
 set ytitle(author) "BLaCkShaDoW"
 set ytitle(website) "wWw.TCLScriptS.NeT"
-set ytitle(version) "v1.6"
+set ytitle(version) "v1.7"
 
-# Languages
+#Languages
 
 # Romanian
 
-set black(ytitle.ro.1) "\002\[YouTube\]\002 Titlu:\002 %msg.1%\002 | Publicat de:\002 %msg.2%\002 | Data:\002 %msg.6%\002 | Vizionari:\002 %msg.3%\002 | Aprecieri:\002 %msg.4%%\002 | Neaprecieri:\002 %msg.5%%\002"
-set black(ytitle.ro.2) "\[\002You\0030,4Tube\003\002\003\] \00310Titlu:\0034 %msg.1% \003| \00310Publicat de:\0034 %msg.2% \003| \00310Data:\0034 %msg.6% \003| \00310Vizionari:\0034 %msg.3% \003| \00310Aprecieri:\0034 %msg.4%% \003| \00310Neaprecieri:\0034 %msg.5%%\003"
+set black(ytitle.ro.1) "\002\[YouTube\]\002 Titlu:\002 %msg.1%\002 | Publicat de:\002 %msg.2%\002 | Durata:\002 %msg.7%m%msg.8%s\002 | Data:\002 %msg.6%\002 | Vizionari:\002 %msg.3%\002 | Aprecieri:\002 %msg.4%%\002 | Neaprecieri:\002 %msg.5%%\002"
+set black(ytitle.ro.2) "\[\002You\0030,4Tube\003\002\003\] \00310Titlu:\0034 %msg.1% \003| \00310Publicat de:\0034 %msg.2% \003| \00310Durata:\0034 %msg.7%m%msg.8%s\003 | \00310Data:\0034 %msg.6% \003| \00310Vizionari:\0034 %msg.3% \003| \00310Aprecieri:\0034 %msg.4%% \003| \00310Neaprecieri:\0034 %msg.5%%\003"
 set black(ytitle.ro.3) "\002\[YouTube\]\002 Acest videoclip nu exista."
 
 # English
 
-set black(ytitle.en.1) "\002\[YouTube\]\002 Title:\002 %msg.1%\002 | Uploaded by:\002 %msg.2%\002 | Date:\002 %msg.6%\002 | Views:\002 %msg.3%\002 | Likes:\002 %msg.4%%\002 | DisLIKEs:\002 %msg.5%%\002"
-set black(ytitle.en.2) "\[\002You\0030,4Tube\003\002\003\] \00310Title:\0034 %msg.1% \003| \00310Uploaded by:\0034 %msg.2% \003| \00310Date:\0034 %msg.6% \003| \00310Views:\0034 %msg.3% \003| \00310Likes:\0034 %msg.4%% \003| \00310DisLIKEs:\0034 %msg.5%%\003"
+set black(ytitle.en.1) "\002\[YouTube\]\002 Title:\002 %msg.1%\002 | Uploaded by:\002 %msg.2%\002 | Length:\002 %msg.7%m%msg.8%s\002 | Date:\002 %msg.6%\002 | Views:\002 %msg.3%\002 | Likes:\002 %msg.4%%\002 | DisLIKEs:\002 %msg.5%%\002"
+set black(ytitle.en.2) "\[\002You\0030,4Tube\003\002\003\] \00310Title:\0034 %msg.1% \003| \00310Uploaded by:\0034 %msg.2% \003| \00310Length:\0034 %msg.7%m%msg.8%s\003 | \00310Date:\0034 %msg.6% \003| \00310Views:\0034 %msg.3% \003| \00310Likes:\0034 %msg.4%% \003| \00310DisLIKEs:\0034 %msg.5%%\003"
 set black(ytitle.en.3) "\002\[YouTube\]\002 This video does not exist."
 
 # French
 
-set black(ytitle.fr.1) "\002\[YouTube\]\002 Titre:\002 %msg.1%\002 | Telecharge par:\002 %msg.2%\002 | La date:\002 %msg.6%\002 | Vues:\002 %msg.3%\002 | J'aime:\002 %msg.4%%\002 | J'aime pas:\002 %msg.5%%\002"
-set black(ytitle.fr.2) "\[\002You\0030,4Tube\003\002\003\] \00310Titre:\0034 %msg.1% \003| \00310Telecharge par:\0034 %msg.2% \003| \00310La date:\0034 %msg.6% \003| \00310Vues:\0034 %msg.3% \003| \00310J'aime:\0034 %msg.4%% \003| \00310J'aime pas:\0034 %msg.5%%\003"
+set black(ytitle.fr.1) "\002\[YouTube\]\002 Titre:\002 %msg.1%\002 | Telecharge par:\002 %msg.2%\002 | Duree:\002 %msg.7%m%msg.8%s\002 La date:\002 %msg.6%\002 | Vues:\002 %msg.3%\002 | J'aime:\002 %msg.4%%\002 | J'aime pas:\002 %msg.5%%\002"
+set black(ytitle.fr.2) "\[\002You\0030,4Tube\003\002\003\] \00310Titre:\0034 %msg.1% \003| \00310Telecharge par:\0034 %msg.2% \003| \00310Duree:\0034 %msg.7%m%msg.8%s\003 | \00310La date:\0034 %msg.6% \003| \00310Vues:\0034 %msg.3% \003| \00310J'aime:\0034 %msg.4%% \003| \00310J'aime pas:\0034 %msg.5%%\003"
 set black(ytitle.fr.3) "\002\[YouTube\]\002 Cette video n'existe pas."
 
 # Spanish
 
-set black(ytitle.es.1) "\002\[YouTube\]\002 Titulo:\002 %msg.1%\002 | Cargado por:\002 %msg.2%\002 | Fecha:\002 %msg.6%\002 | Visualizaciones:\002 %msg.3%\002 | Gustos:\002 %msg.4%%\002 | Disgustos:\002 %msg.5%%\002"
-set black(ytitle.es.2) "\[\002You\0030,4Tube\003\002\003\] \00310Titulo:\0034 %msg.1% \003| \00310Cargado por:\0034 %msg.2% \003| \00310Fecha:\0034 %msg.6% \003| \00310Visualizaciones:\0034 %msg.3% \003| \00310Gustos:\0034 %msg.4%% \003| \00310Disgustos:\0034 %msg.5%%\003"
+set black(ytitle.es.1) "\002\[YouTube\]\002 Titulo:\002 %msg.1%\002 | Cargado por:\002 %msg.2%\002 | Duracion:\002 %msg.7%m%msg.8%s\002 | Fecha:\002 %msg.6%\002 | Visualizaciones:\002 %msg.3%\002 | Gustos:\002 %msg.4%%\002 | Disgustos:\002 %msg.5%%\002"
+set black(ytitle.es.2) "\[\002You\0030,4Tube\003\002\003\] \00310Titulo:\0034 %msg.1% \003| \00310Cargado por:\0034 %msg.2% \003| \00310Duracion:\0034 %msg.7%m%msg.8%s\003 | \00310Fecha:\0034 %msg.6% \003| \00310Visualizaciones:\0034 %msg.3% \003| \00310Gustos:\0034 %msg.4%% \003| \00310Disgustos:\0034 %msg.5%%\003"
 set black(ytitle.es.3) "\002\[YouTube\]\002 Este video no existe."
 
 # Italian
 
-set black(ytitle.it.1) "\002\[YouTube\]\002 Titolo:\002 %msg.1%\002 | Caricato da:\002 %msg.2%\002 | Data:\002 %msg.6%\002 | Visualizzazioni:\002 %msg.3%\002 | Piace:\002 %msg.4%%\002 | Non mi piace:\002 %msg.5%%\002"
-set black(ytitle.it.2) "\[\002You\0030,4Tube\003\002\003\] \00310Titolo:\0034 %msg.1% \003| \00310Caricato da:\0034 %msg.2% \003| \00310Data:\0034 %msg.6% \003| \00310Visualizzazioni:\0034 %msg.3% \003| \00310Piace:\0034 %msg.4%% \003| \00310Non mi piace:\0034 %msg.5%%\003"
+set black(ytitle.it.1) "\002\[YouTube\]\002 Titolo:\002 %msg.1%\002 | Caricato da:\002 %msg.2%\002 | Durata:\002 %msg.7%m%msg.8%s\002 | Data:\002 %msg.6%\002 | Visualizzazioni:\002 %msg.3%\002 | Piace:\002 %msg.4%%\002 | Non mi piace:\002 %msg.5%%\002"
+set black(ytitle.it.2) "\[\002You\0030,4Tube\003\002\003\] \00310Titolo:\0034 %msg.1% \003| \00310Caricato da:\0034 %msg.2% \003| \00310Durata:\0034 %msg.7%m%msg.8%s\003 | \00310Data:\0034 %msg.6% \003| \00310Visualizzazioni:\0034 %msg.3% \003| \00310Piace:\0034 %msg.4%% \003| \00310Non mi piace:\0034 %msg.5%%\003"
 set black(ytitle.it.3) "\002\[YouTube\]\002 Questo video non esiste."
 
 putlog "\002$ytitle(projectName) $ytitle(version)\002 coded by $ytitle(author) ($ytitle(website)): Loaded."
