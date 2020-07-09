b5cf8db49771c3588110af7e16f42c1cbd822d9e
Daniel Voipan <voipandaniel@gmail.com>
2020-07-09 18:05:06 +0300

    Revert to Youtube Title 1.9 for now. Working ok.

--- YoutubeTitle.tcl
+++ YoutubeTitle.tcl
@@ -1,17 +1,12 @@
 ##########################################################################################
-# Youtube Title 2.2
+# Youtube Title 1.9
 # - fetches and displays video information when a YouTube link is posted in channel.
-# - displays title, date and rating of posted video links.
+# - displays title, date and rating of posted video links. 
 # - supports also HTTPS links.
 #
-# requires: packages http, tls
+# requires: packages http 
 #
 # UPDATES/CHANGES:
-# - (2.2) added german language
-# - (2.2) solved some little bugs
-# - (2.2) solved issues related to youtube changes
-# - (2.1) solved issue with youtube live videos
-# - (2.0) now the script access directly Youtube
 # - (1.9) added stars for like display instead of procentages
 # - (1.8) added anti-flood support
 # - (1.8) added youtube search (it will get info from the first youtube video)
@@ -31,12 +26,12 @@
 # To work put the http.tcl, from the archive, in your eggdrop config (if you don't have it instaled)
 #
 #                       BLaCkShaDoW ProductionS
-#      _   _   _   _   _   _   _   _   _   _   _   _   _   _
-#     / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \
+#      _   _   _   _   _   _   _   _   _   _   _   _   _   _  
+#     / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ 
 #    ( t | c | l | s | c | r | i | p | t | s | . | n | e | t )
 #     \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/
 #                                    #TCL-HELP @ Undernet.org
-#
+#     
 ##########################################################################################
 
 ###
@@ -62,7 +57,7 @@ set ytitle(youtube_search_cmd) "yt"
 #
 ###
 
-set ytitle(youtube_search_flags) "-|-"
+set ytitle(youtube_search_flags) "mn|AOM"
 
 ###
 # FLOOD PROTECTION
@@ -81,21 +76,20 @@ set ytitle(flood_prot) "3:10"
 ###
 # Language setting
 # - what language you want to receive the youTube data
-#   ( RO / EN / ES / FR / IT / DE )
+#   ( RO / EN / ES / FR / IT )
 #
-set ytitle(default_lang) "DE"
+set ytitle(default_lang) "RO"
 
 ###
 # Colors setting
 # - what format you want to receive the youTube data
 #   (1) Enable or (0) disable colors
 #
-set ytitle(colors) "0"
+set ytitle(colors) "1"
 
 ##########################################################################################
 
 package require http
-package require tls
 
 ###
 # Bindings
@@ -116,118 +110,6 @@ setudef flag ytsearch
 # Do NOT touch unless you know what you are doing
 ###
 
-	set counter_link "ytitle_link_counter"
-if {![file exists $counter_link]} {
-	set file [open $counter_link w]
-	puts $file "0"
-	close $file
-}
-
-###
-proc ytitle:counter {} {
-	global ytitle counter_link
-	set file [open $counter_link r]
-	set read_it [read -nonewline $file]
-	close $file
-	set incr_num [expr $read_it + 1]
-	set file [open $counter_link w]
-	puts $file $incr_num
-	close $file
-	return $incr_num
-}
-
-###
-proc tls:socket args {
-   set opts [lrange $args 0 end-2]
-   set host [lindex $args end-1]
-   set port [lindex $args end]
-   ::tls::socket -servername $host {*}$opts $host $port
-}
-
-###
-proc ytitle:get_idsearch {arg} {
-	global ytitle
-	set text [join $arg "+"]
-	set search_query [::http::formatQuery search_query $text]
-	set url "https://www.youtube.com/results?$search_query"
-	set id "-1"
-	http::register https 443 tls::socket
-	set ipq [http::config -useragent "lynx"]
-	set ipq [::http::geturl "$url" -timeout 30000]
-	set status [http::status $ipq]
-if {$status != "ok"} {
-	::http::cleanup $ipq
-	return 0
-}
-	set data [http::data $ipq]
-	regexp {<li><div class="yt-lockup yt-lockup-tile yt-lockup-video vve-check clearfix" data-context-item-id="(.*)"} $data -> id
-if {$id == -1} {
-	regexp {\[\{\"videoRenderer\"\:\{\"videoId\"\:\"(.*?)\"} $data -> id
-}
-if {$id == "-1"} {
-	return 0
-}
-	return [string map {"\"" ""} [lindex [split $id] 0]]
-}
-
-###
-proc ytitle:data {link} {
-	global ytitle
-	set valid_link [regexp {(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})} $link -> id]
-if {![info exists id]} {
-	return 0
-}
-	set url "https://www.youtube.com/watch?v=$id"
-	set short_url "https://youtu.be/$id"
-	http::register https 443 tls::socket
-	set ipq [http::config -useragent "lynx"]
-	set ipq [::http::geturl "$url" -timeout 30000]
-	set status [http::status $ipq]
-if {$status != "ok"} {
-	::http::cleanup $ipq
-	return 0
-}
-	set link_counter [ytitle:counter]
-	set data [split [http::data $ipq] "\n"]
-	set title ""
-	set date ""
-	set view_count ""
-	set likes ""
-	set dislikes ""
-	set bywho ""
-	set duration ""
-	set tools ""
-	set type 0
-foreach line $data {
-	regexp {"name": "(.*)"} $line -> bywho
-	regexp {<span id="eow-title" class="watch-title" dir="ltr" title="(.*)">} $line -> title
-if {$title == ""} {
-	regexp {<meta name="title" content="(.*)">} $line -> title
-}
-	regexp {<meta itemprop="datePublished" content="(.*)">} $line -> date
-	regexp {<div class="watch-view-count">(.*)</div>} $line -> view_count
-if {$view_count == ""} {
-	regexp {\"viewCount\":\{\"simpleText\":\"(.*?)\"\}} $line -> view_count
-	regexp {\"likeStatus\":\"INDIFFERENT\"\,\"tooltip\"\:\"(.*?)\"\}\}} $line -> tools
-if {$tools != ""} {
-	set type 1
-}
-	set split_tools [split $tools "/"]
-	set likes [string map {"." ""} [concat [lindex $split_tools 0]]]
-	set dislikes [string map {"." ""} [concat [lindex $split_tools 1]]]
-} else {
-	regexp {<div class="video-extras-sparkbar-likes" style="width:(.*)"></div>} $line -> likes
-	regexp {div class="video-extras-sparkbar-dislikes" style="width:(.*)"><\/div>} $line -> dislikes
-	set likes [concat $likes]
-	set dislikes [concat $dislikes]
-	set view_count [lindex [split $view_count " "] 0]
-}
-	regexp {<meta itemprop="duration" content="(.*)">} $line -> duration
-}
-	::http::cleanup $ipq
-	return [list $title $view_count $likes $dislikes $bywho $date $duration $short_url $link_counter]
-}
-
 ###
 proc search:youtube {nick host hand chan arg} {
 if {![channel get $chan ytsearch]} {
@@ -240,15 +122,9 @@ if {$flood_protect == "1"} {
 	set text [lrange [split $arg] 0 end]
 if {$text == ""} {
 	return
-}
-	set text [join $text "+"]
-	set get_id [ytitle:get_idsearch $text]
-if {$get_id == "-1"} {
-	youtube:tell $nick $chan 0 1
-} else {
-	set url "https://www.youtube.com/watch?v=$get_id"
-	youtube:get:title $url $nick $chan 1
 	}
+	set text [join $text "+"]
+	youtube:get:title $text $nick $chan 1
 }
 
 ###
@@ -273,11 +149,15 @@ if {[string match -nocase "*youtube.com/watch*" $youtube_link] || [string match 
 proc youtube:get:title {link nick chan type} {
 	set novideo 0
 	set live 0
-	set output [ytitle:data $link]
-if {$output == "0"} {
-	youtube:tell $nick $chan 0 1
-	return
+	set ipq [http::config -useragent "lynx"]
+if {$type == "0"} {
+	set ipq [http::geturl "http://youtubesongname.000webhostapp.com/index2.php?link=$link" -timeout 50000]
+} else {
+	set ipq [http::geturl "http://youtubesongname.000webhostapp.com/index2.php?search=$link" -timeout 50000]
 }
+	set getipq [http::data $ipq]
+	set output [split $getipq "\n"]
+	http::cleanup $ipq
 	set title [string map { "&amp;" "&"
 			"&#39;" "'"
 			"&quot;" "\""
@@ -286,8 +166,11 @@ if {$output == "0"} {
 if {$title == ""} { set novideo 1}
 	set views [lindex $output 1]
 if {$views == ""} { set views "N/A" }
+	set split_views [split $views " "]
+	set views [lindex $split_views 0]
+	set views [string map {"&nbsp;" "."} $views]
 	set likes [lindex $output 2]
-	set dontlike [lindex $output 3]
+	set dontlike [concat [lindex $output 3]]
 	set bywho [concat [lindex $output 4]]
 	set update [lindex $output 5]
 	set duration [lindex $output 6]
@@ -296,6 +179,10 @@ if {$views == ""} { set views "N/A" }
 	set duration [string map {"PT" ""} $duration]
 	set minutes [lindex [split $duration "M"] 0]
 	set seconds [concat [string map {"S" ""} [lindex [split $duration "M"] 1]]]
+if {$novideo == "1"} {
+	youtube:tell $nick $chan 0 1
+	return
+}
 	set scan [clock scan $update -format {%Y-%m-%d}]
 	set update [clock format $scan -format {%d/%m/%Y}]
 	set like_bar [youtube:like_bar $likes $dontlike $chan]
@@ -335,7 +222,7 @@ if {$setcolor == "1"} {
 if {[info exists black(ytitle.$getlang.$type)]} {
 	set reply [string map [array get replace] $black(ytitle.$getlang.$type)]
 	putserv "PRIVMSG $chan :$reply"
-		}
+		}	
 	}
 }
 
@@ -344,10 +231,14 @@ proc youtube:getlang {chan} {
 	global black ytitle
 	set getlang [string tolower [channel get $chan ytlang]]
 if {$getlang == ""} {
-	set lang $ytitle(default_lang)
-		} else {
+	set lang "en"
+} else {
+if {[info exists black(ytitle.$getlang.1)]} {
 	set lang $getlang
+} else { 
+	set lang $ytitle(default_lang)
 		}
+	}
 	return [string tolower $lang]
 }
 
@@ -382,11 +273,11 @@ if {[string match "*youtube:remove:flood $host $chan*" [join [lindex $tmr 1]]]} 
 	killutimer [lindex $tmr 2]
 	}
 }
-if {![info exists ytitle(flood:$host:$chan)]} {
-	set ytitle(flood:$host:$chan) 0
+if {![info exists ytitle(flood:$host:$chan)]} { 
+	set ytitle(flood:$host:$chan) 0 
 }
 	incr ytitle(flood:$host:$chan)
-	utimer $timer [list youtube:remove:flood $host $chan]
+	utimer $timer [list youtube:remove:flood $host $chan]	
 if {$ytitle(flood:$host:$chan) > $number} {
 	set ytitle(flood:$host:$chan:act) 1
 	utimer [expr $ytitle(ignore_prot) * 60] [list youtube:expire:flood $host $chan]
@@ -416,50 +307,23 @@ if {[info exists ytitle(flood:$host:$chan:act)]} {
 set ytitle(projectName) "Youtube Title"
 set ytitle(author) "BLaCkShaDoW"
 set ytitle(website) "wWw.TCLScriptS.NeT"
-set ytitle(version) "v2.2"
-
-###
-proc youtube:percentage {total number} {
-	if {$number == 0} { return 0}
-  if {$total > 0} {
-	if {$total > 100} {
-   return [expr $number / [expr $total / 100]];
- } else {
-	 return [expr $number / [expr 100 / $total]];
- 				}
-  		} else {
-    return 0;
-  }
-}
+set ytitle(version) "v1.9"
 
 ###
 proc youtube:like_bar {like dislike chan} {
 	global ytitle
-	set setcolor [youtube:getcolor $chan]
-if {$like == "" && $dislike == ""} {
-if {$setcolor == "1"} {
-	return "\00304N/A\003"
-} else {
-	return "\002N/A\002"
-	}
-}
-if {![regexp {%} $like]} {
-		set sum [expr $like + $dislike]
-		set like [youtube:percentage $sum $like]
-		set dislike [youtube:percentage $sum $dislike]
-} else {
 	set like [string map {"%" ""} $like]
 	set dislike [string map {"%" ""} $dislike]
-}
-
+	set setcolor [youtube:getcolor $chan]
 if {$like > $dislike} {
-	set dif [expr $like - $dislike]
+	set dif [expr ($like - $dislike)]
 } else {
-	set dif [expr $dislike - $like]
+	set dif [expr ($dislike - $like)]
 }
 	set like_star "&#9733"
 	set dislike_star "&#9734"
 	set dif [expr {double(round(1000*$dif))/1000.0}]
+
 if {[expr {round($dif / 10)}] > 10} {
 	set red 10
 	set green [expr {10 - $red}]
@@ -506,21 +370,21 @@ set black(ytitle.ro.5) "\[\002You\0030,4Tube\003\002\003\] \00310Titlu:\0034 %ms
 set black(ytitle.en.1) "\002\[YouTube\]\002 Title:\002 %msg.1%\002 | Uploaded by:\002 %msg.2%\002 | Length:\002 %msg.7%m%msg.8%s\002 | Date:\002 %msg.6%\002 | Views:\002 %msg.3%\002 | Like: %msg.4% - \[\002%msg.9%\002\] -"
 set black(ytitle.en.2) "\[\002You\0030,4Tube\003\002\003\] \00310Title:\0034 %msg.1% \003| \00310Uploaded by:\0034 %msg.2% \003| \00310Length:\0034 %msg.7%m%msg.8%s\003 | \00310Date:\0034 %msg.6% \003| \00310Views:\0034 %msg.3% \003| \00310Like:\003 %msg.4% - \[\00304%msg.9%\003\] -"
 set black(ytitle.en.3) "\002\[YouTube\]\002 This video does not exist."
-set black(ytitle.en.4) "\002\[YouTube\]\002 Title:\002 %msg.1%\002 | Uploaded by:\002 %msg.2%\002 | Length:\002 %msg.7%m%msg.8%s\002 | Date:\002 %msg.6%\002 | Views:\002 %msg.3%\002 | Like: %msg.4% | Link:\002 %msg.10%\002 - \[\002%msg.9%\002\] -"
+set black(ytitle.en.4) "\002\[YouTube\]\002 Title:\002 %msg.1%\002 | Uploaded by:\002 %msg.2%\002 | Length:\002 %msg.7%m%msg.8%s\002 | Date:\002 %msg.6%\002 | Views:\002 %msg.3%\002 | Like: %msg.4% | Link:\002 %msg.10%\002- \[\002%msg.9%\002\] -"
 set black(ytitle.en.5) "\[\002You\0030,4Tube\003\002\003\] \00310Title:\0034 %msg.1% \003| \00310Uploaded by:\0034 %msg.2% \003| \00310Length:\0034 %msg.7%m%msg.8%s\003 | \00310Date:\0034 %msg.6% \003| \00310Views:\0034 %msg.3% \003| \00310Like:\003 %msg.4% | \00310Link: \0034%msg.10%\003 - \[\00304%msg.9%\003\] -"
 # French
 
 set black(ytitle.fr.1) "\002\[YouTube\]\002 Titre:\002 %msg.1%\002 | Telecharge par:\002 %msg.2%\002 | Duree:\002 %msg.7%m%msg.8%s\002 La date:\002 %msg.6%\002 | Vues:\002 %msg.3%\002 | J'aime: %msg.4% - \[\002%msg.9%\002\] -"
 set black(ytitle.fr.2) "\[\002You\0030,4Tube\003\002\003\] \00310Titre:\0034 %msg.1% \003| \00310Telecharge par:\0034 %msg.2% \003| \00310Duree:\0034 %msg.7%m%msg.8%s\003 | \00310La date:\0034 %msg.6% \003| \00310Vues:\0034 %msg.3% \003| \00310J'aime:\003 %msg.4% - \[\00304%msg.9%\003\] -"
 set black(ytitle.fr.3) "\002\[YouTube\]\002 Cette video n'existe pas."
-set black(ytitle.fr.4) "\002\[YouTube\]\002 Titre:\002 %msg.1%\002 | Telecharge par:\002 %msg.2%\002 | Duree:\002 %msg.7%m%msg.8%s\002 La date:\002 %msg.6%\002 | Vues:\002 %msg.3%\002 | J'aime: %msg.4% | Lien:\002 %msg.10%\002 - \[\002%msg.9%\002\] -"
+set black(ytitle.fr.4) "\002\[YouTube\]\002 Titre:\002 %msg.1%\002 | Telecharge par:\002 %msg.2%\002 | Duree:\002 %msg.7%m%msg.8%s\002 La date:\002 %msg.6%\002 | Vues:\002 %msg.3%\002 | J'aime: %msg.4% | Lien:\002 %msg.10%\002- \[\002%msg.9%\002\] -"
 set black(ytitle.fr.5) "\[\002You\0030,4Tube\003\002\003\] \00310Titre:\0034 %msg.1% \003| \00310Telecharge par:\0034 %msg.2% \003| \00310Duree:\0034 %msg.7%m%msg.8%s\003 | \00310La date:\0034 %msg.6% \003| \00310Vues:\0034 %msg.3% \003| \00310J'aime:\003 %msg.4% | \00310Lien: \0034%msg.10%\003 - \[\00304%msg.9%\003\] -"
 # Spanish
 
 set black(ytitle.es.1) "\002\[YouTube\]\002 Titulo:\002 %msg.1%\002 | Cargado por:\002 %msg.2%\002 | Duracion:\002 %msg.7%m%msg.8%s\002 | Fecha:\002 %msg.6%\002 | Visualizaciones:\002 %msg.3%\002 | Gustos: %msg.4% - \[\002%msg.9%\002\] -"
 set black(ytitle.es.2) "\[\002You\0030,4Tube\003\002\003\] \00310Titulo:\0034 %msg.1% \003| \00310Cargado por:\0034 %msg.2% \003| \00310Duracion:\0034 %msg.7%m%msg.8%s\003 | \00310Fecha:\0034 %msg.6% \003| \00310Visualizaciones:\0034 %msg.3% \003| \00310Gustos:\003 %msg.4% - \[\00304%msg.9%\003\] -"
 set black(ytitle.es.3) "\002\[YouTube\]\002 Este video no existe."
-set black(ytitle.es.4) "\002\[YouTube\]\002 Titulo:\002 %msg.1%\002 | Cargado por:\002 %msg.2%\002 | Duracion:\002 %msg.7%m%msg.8%s\002 | Fecha:\002 %msg.6%\002 | Visualizaciones:\002 %msg.3%\002 | Gustos: %msg.4% | Enlazar:\002 %msg.10%\002 - \[\002%msg.9%\002\] -"
+set black(ytitle.es.4) "\002\[YouTube\]\002 Titulo:\002 %msg.1%\002 | Cargado por:\002 %msg.2%\002 | Duracion:\002 %msg.7%m%msg.8%s\002 | Fecha:\002 %msg.6%\002 | Visualizaciones:\002 %msg.3%\002 | Gustos: %msg.4% | Enlazar:\002 %msg.10%\002- \[\002%msg.9%\002\] -"
 set black(ytitle.es.5) "\[\002You\0030,4Tube\003\002\003\] \00310Titulo:\0034 %msg.1% \003| \00310Cargado por:\0034 %msg.2% \003| \00310Duracion:\0034 %msg.7%m%msg.8%s\003 | \00310Fecha:\0034 %msg.6% \003| \00310Visualizaciones:\0034 %msg.3% \003| \00310Gustos:\003 %msg.4% | \00310Enlazar: \0034%msg.10%\003 - \[\00304%msg.9%\003\] -"
 # Italian
 
@@ -529,14 +393,6 @@ set black(ytitle.it.2) "\[\002You\0030,4Tube\003\002\003\] \00310Titolo:\0034 %m
 set black(ytitle.it.3) "\002\[YouTube\]\002 Questo video non esiste."
 set black(ytitle.it.4) "\002\[YouTube\]\002 Titolo:\002 %msg.1%\002 | Caricato da:\002 %msg.2%\002 | Durata:\002 %msg.7%m%msg.8%s\002 | Data:\002 %msg.6%\002 | Visualizzazioni:\002 %msg.3%\002 | Piace: %msg.4% | Collegamento:\002 %msg.10%\002 - \[\002%msg.9%\002\] -"
 set black(ytitle.it.5) "\[\002You\0030,4Tube\003\002\003\] \00310Titolo:\0034 %msg.1% \003| \00310Caricato da:\0034 %msg.2% \003| \00310Durata:\0034 %msg.7%m%msg.8%s\003 | \00310Data:\0034 %msg.6% \003| \00310Visualizzazioni:\0034 %msg.3% \003| \00310Piace:\003 %msg.4% | \00310Collegamento: \0034%msg.10%\003 - \[\00304%msg.9%\003\] -"
-
-# german
-
-set black(ytitle.de.1) "\002\[YouTube\]\002 Titel:\002 %msg.1%\002 | hochgeladen von:\002 %msg.2%\002 | Laenge:\002 %msg.7%m%msg.8%s\002 | Datum:\002 %msg.6%\002 | Aufrufe:\002 %msg.3%\002 | Like: %msg.4% - \[\002%msg.9%\002\] -"
-set black(ytitle.de.2) "\[\002You\0030,4Tube\003\002\003\] \00310Title:\0034 %msg.1% \003| \00310Uploaded by:\0034 %msg.2% \003| \00310Length:\0034 %msg.7%m%msg.8%s\003 | \00310Datum:\0034 %msg.6% \003| \00310Aufrufe:\0034 %msg.3% \003| \00310Like:\003 %msg.4% - \[\00304%msg.9%\003\] -"
-set black(ytitle.de.3) "\002\[YouTube\]\002 Das Video existiert nicht."
-set black(ytitle.de.4) "\002\[YouTube\]\002 Titel:\002 %msg.1%\002 | hochgeladen von:\002 %msg.2%\002 | Laenge:\002 %msg.7%m%msg.8%s\002 | Datum:\002 %msg.6%\002 | Aufrufe:\002 %msg.3%\002 | Like: %msg.4% | Link:\002 %msg.10%\002 - \[\002%msg.9%\002\] -"
-set black(ytitle.de.5) "\[\002You\0030,4Tube\003\002\003\] \00310Titel:\0034 %msg.1% \003| \00310hochgeladen von:\0034 %msg.2% \003| \00310Laenge:\0034 %msg.7%m%msg.8%s\003 | \00310Datum:\0034 %msg.6% \003| \00310Aufrufe:\0034 %msg.3% \003| \00310Like:\003 %msg.4% | \00310Link: \0034%msg.10%\003 - \[\00304%msg.9%\003\] -"
 
 putlog "\002$ytitle(projectName) $ytitle(version)\002 coded by $ytitle(author) ($ytitle(website)): Loaded."
 
